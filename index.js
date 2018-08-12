815d6587723e535d10d1c2a9e5cf9c5216eaa7fc
Oomkar Pandit <oomkar@yahoo.com>
2018-08-12 16:08:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
   return object
 }
 function deleteFromObjectByKey(object,key){
-  delete object[key]
-  return object
+  var newobj=Object.assign({},object)
+  delete newobj[key]
+  return newobj
 }
